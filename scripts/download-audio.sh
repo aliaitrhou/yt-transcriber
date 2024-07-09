43ce6ef9ac68f47d6bbb61a816bad14c6f798ba1
Ali <146731444+aliaitrhou@users.noreply.github.com>
2024-07-09 18:39:22 +0100

    insure yt-dlp exist

--- scripts/download-audio.sh
+++ scripts/download-audio.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Download yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./yt-dlp
+chmod +x ./yt-dlp
+
 VIDEIO_ID=$1
 
 [ -z "$VIDEIO_ID" ] && echo "ERROR: No video ID specified" && exit 1
