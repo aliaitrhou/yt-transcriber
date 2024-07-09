77c8683fd71a9248e90b236701dcd0039bb4787d
Ali <146731444+aliaitrhou@users.noreply.github.com>
2024-07-09 18:06:05 +0100

    clean-up python scripts

--- scripts/translate.py
+++ scripts/translate.py
@@ -1,47 +1,14 @@
-# if you wanna use open-ai (paid):
 from transformers import MarianMTModel, MarianTokenizer
 import os
 import sys
-import openai
 import pysrt
-from openai import OpenAI
-client = OpenAI()
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
 input_data = sys.stdin.read()
 subs = pysrt.from_string(input_data)
 
-# prompt_base = (
-#     "You are going to be a good translator. "
-#     "Here is a part of the transcript of my vlog. "
-#     "Translate the following text precisely into Moroccan Darja "
-#     "Translate from [START] to [END]:\n[START]"
-# )
-#
-#
-# def translate_text(text):
-#     prompt = prompt_base
-#     prompt += text + "\n[END]"
-#     response = client.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         max_tokens=3000,
-#         temperature=0,
-#         messages=[
-#             {"role": "system", "content": prompt},
-#         ]
-#     )
-#     return response.choices[0].message.strip()
-#
-#
-# for index, subtitle in enumerate(subs):
-#     subtitle.text = translate_text(subtitle.text)
-#     print(subtitle)
-
-
-# if you wanna use Hugging-face (free)
+# if you wanna use Hugging-face 
 # Choose the model and tokenizer
 model_name = "Helsinki-NLP/opus-mt-en-ar"
-
 
 def translate_text(text):
     tokenizer = MarianTokenizer.from_pretrained(
