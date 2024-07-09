c09747f77351cc29eb92c1fb49efb9886ff208b7
Ali <146731444+aliaitrhou@users.noreply.github.com>
2024-07-09 18:46:47 +0100

    fix yt-dlp uninstall download-audio.sh

--- scripts/download-audio.sh
+++ scripts/download-audio.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 # Download yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./yt-dlp
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O ./yt-dlp
+
+# Make it executable
 chmod +x ./yt-dlp
 
 VIDEIO_ID=$1
