14fc15509ca07da48ccda0b3b9d5369e759f52fc
aliaitrahou <alirhou7@gmail.com>
2025-02-14 22:03:02 +0100

    fix(production): YouTube blocking requests due to rate limits

--- scripts/download-audio.sh
+++ scripts/download-audio.sh
@@ -6,4 +6,12 @@ VIDEIO_ID=$1
 
 mkdir -p ./tmp
 
-yt-dlp "https://youtube.com/watch?v=$VIDEIO_ID" --format m4a -o "./tmp/%(id)s.%(ext)s" 2>&1
+yt-dlp "https://youtube.com/watch?v=$VIDEO_ID" --format m4a -o "./tmp/%(id)s.%(ext)s" --cookies-from-browser chrome 2>&1
+
+# Check if the download was successful
+if [ $? -ne 0 ]; then
+  echo "ERROR: Failed to download video."
+  exit 1
+fi
+
+echo "Download completed successfully!"
