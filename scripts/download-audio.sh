f1b6b3c73ca518b637307709cbd769207ee3bc89
aliaitrahou <alirhou7@gmail.com>
2025-02-15 16:23:54 +0100

    fix(yt-dlp): the package doesn't function in production

--- scripts/download-audio.sh
+++ scripts/download-audio.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-VIDEIO_ID=$1
+VIDEO_ID=$1
 
-[ -z "$VIDEIO_ID" ] && echo "ERROR: No video ID specified" && exit 1
+[ -z "$VIDEO_ID" ] && echo "ERROR: No video ID specified" && exit 1
 
-mkdir -p ./tmp
-
-yt-dlp "https://youtube.com/watch?v=$VIDEO_ID" --format m4a -o "./tmp/%(id)s.%(ext)s" --cookies cookies.txt 2>&1
-
-# Check if the download was successful
-if [ $? -ne 0 ]; then
-  echo "ERROR: Failed to download video."
-  exit 1
-fi
-
-echo "Download completed successfully!"
+yt-dlp "https://www.youtube.com/watch?v=$VIDEO_ID" --format m4a -o "./tmp/%(id)s.%(ext)s" --cookies cookies.txt 2>&1
