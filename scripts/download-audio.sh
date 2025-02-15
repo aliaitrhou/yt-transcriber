9b4dfe793e8ac9e6a4fece45a9bc4bced7ebda86
aliaitrahou <alirhou7@gmail.com>
2025-02-15 17:55:36 +0100

    fix(bash-script): yt-dlp and cookies

--- scripts/download-audio.sh
+++ scripts/download-audio.sh
@@ -4,4 +4,6 @@ VIDEO_ID=$1
 
 [ -z "$VIDEO_ID" ] && echo "ERROR: No video ID specified" && exit 1
 
-yt-dlp "https://www.youtube.com/watch?v=$VIDEO_ID" --format m4a -o "./tmp/%(id)s.%(ext)s" --cookies cookies.txt 2>&1
+yt-dlp "https://www.youtube.com/watch?v=$VIDEO_ID" \
+  --format m4a -o "./tmp/%(id)s.%(ext)s" \
+  --cookies cookies.txt --verbose 2>&1
