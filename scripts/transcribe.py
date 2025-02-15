03e4d4cacb3f9670a2ce953870e6d7b2720738b6
aliaitrahou <alirhou7@gmail.com>
2025-02-15 15:29:00 +0100

    refactor(scripts): change scripts to use openai and together-ai

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -1,41 +1,21 @@
 import os
+import openai
+from dotenv import load_dotenv
 import sys
-import textwrap
-import whisper
-model = whisper.load_model("base")
-video_id = sys.argv[1]
-audio_file_path = os.path.join(os.getcwd(), 'tmp', video_id + '.m4a')
-print(audio_file_path)
-text = model.transcribe(audio_file_path)
 
-# this function is just a way to convert the plain text to SRT like format.
+load_dotenv()
 
+openai.api_key = os.getenv('OPENAI_API_KEY')
+audio_filename = sys.argv[1]
+audio_file_path = os.path.join(os.getcwd(), 'tmp', audio_filename)
 
-def text_to_srt(transcription):
-    subtitles = []
-    words_per_line = 40
-    chars_per_second = 50
-    paragraphs = transcription.strip().split("\n")
-    lines = []
-    for paragraph in paragraphs:
-        lines.extend(paragraph.split(". "))
-
-    current_time = 0
-    for i, line in enumerate(lines):
-        words = line.split()
-        wrapped_lines = textwrap.wrap(" ".join(words), width=words_per_line)
-
-        for wrapped_line in wrapped_lines:
-            duration_seconds = max(1, len(wrapped_line) / chars_per_second)
-
-            start_time = f"00:00:{current_time:02d},000"
-            end_time = f"00:00:{current_time + duration_seconds:.3f}"
-            subtitle = f" {start_time} --> {end_time}\n{wrapped_line}\n"
-            subtitles.append(subtitle)
-            current_time += duration_seconds
-
-    return "".join(subtitles)
-
-
-srt_text = text_to_srt(text["text"])
-print(srt_text, flush=True)
+audio_file = open(audio_file_path, 'rb')
+transcript = openai.audio.transcriptions.create(
+    file=audio_file,
+    model="whisper-1",
+    response_format="srt",
+    prompt=(
+        'I am a software developer. My name is Ali. '
+    )
+)
+print(transcript)
--- scripts/translate.py
+++ scripts/translate.py
@@ -1,23 +1,42 @@
-from transformers import MarianMTModel, MarianTokenizer
+import os
 import sys
+from together import Together
+from dotenv import load_dotenv
 import pysrt
+
+load_dotenv()
+
+client = Together(
+    api_key=os.getenv("TOGETHER_API_KEY"))
 
 input_data = sys.stdin.read()
 subs = pysrt.from_string(input_data)
-# if you wanna use Hugging-face (free)
-model_name = "Helsinki-NLP/opus-mt-en-ar"
+
+prompt_base = (
+    """you are going to be a great translator.
+    Here is a part of the transcript of a youtube video.
+    Your role is  to Translate the following text precisely into Arabic.
+    Translate from [START] to [END]:\n[START]\n"""
+)
 
 
 def translate_text(text):
-    tokenizer = MarianTokenizer.from_pretrained(
-        model_name, direction="rtl", add_prefix_space=True)
+    prompt = prompt_base
+    prompt += text + "\n[END]"
+    response = client.chat.completions.create(
+        model="meta-llama/Llama-3.3-70B-Instruct-Turbo", max_tokens=3000,
+        temperature=0,
+        messages=[
+            {"role": "user", "content": prompt}],
+    )
 
-    model = MarianMTModel.from_pretrained(model_name)
-    inputs = tokenizer(text, return_tensors="pt", truncation=True)
-    outputs = model.generate(**inputs)
-    response = tokenizer.decode(
-        outputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
-    return response
+    translated = response.choices[0].message.content
+    if translated.startswith('「'):
+        translated = translated[1:]
+        if translated.endswith('」'):
+            translated = translated[:-1]
+
+    return translated
 
 
 for index, subtitle in enumerate(subs):
